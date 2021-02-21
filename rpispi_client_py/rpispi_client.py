e798b72ba2bdeb231730a16813efc17423c5b95a
Scott Nortman <scott.nortman@gmail.com>
2021-02-21 18:48:18 -0500

    WIP

--- rpispi_client_py/rpispi_client.py
+++ rpispi_client_py/rpispi_client.py
@@ -1,3 +1,17 @@
+import sys
+
+from rpispi.srv import Spixfer
+import rclpy
+from rclpy.node import Node
+
+class RpiSpiClientAsyc( Node ):
+    
+    def __init__( self ):
+        super().__init__('rpi_spi_client_async')
+        self.cli = self.create_client(Spixfer, 'spi')
+
+
+
 def main():
     print('Hi from rpispi_client_py.')
 
